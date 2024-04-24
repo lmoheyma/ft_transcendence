1b7844b72cae8bb362ddf37224eed654600982a8
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2024-04-24 14:56:36 +0200

    Converting WebSocketConsumer to AsyncWebsocketConsumer

--- backend/rest_api/pongrooms/consumers.py
+++ backend/rest_api/pongrooms/consumers.py
@@ -1,9 +1,9 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
-class   PongConsumer(WebsocketConsumer):
-    def connect(self):
+class   PongConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.player_no = self.scope["url_route"]["kwargs"]["player_no"]
         if self.player_no != 1 and self.player_no != 2:
@@ -11,22 +11,22 @@ class   PongConsumer(WebsocketConsumer):
         self.versus_no = 1 if self.player_no == 2 else 2
         self.adversary_name = f"player{self.versus_no}_{self.room_name}"
         self.player_name    = f"player{self.player_no}_{self.room_name}"
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.player_name, self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
             self.player_name, self.channel_name
         )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         try :
             obj = json.loads(text_data)
         except :
             obj = {'type' : 'error', 'message' : 'Invalid JSON'}
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.adversary_name,
             {
                 "type" : "send_packet",
@@ -34,6 +34,6 @@ class   PongConsumer(WebsocketConsumer):
             }
         )
 
-    def send_packet(self, event):
+    async def send_packet(self, event):
         message = event["message"]
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message}))
