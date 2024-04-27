d9d15d4a42c3cde28be377642825a30ff314ab15
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2024-04-27 23:34:55 +0200

    Added Postgresql container

--- backend/rest_api/rest_api/settings.py
+++ backend/rest_api/rest_api/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'transcendence_db',
+        'USER' : 'django',
+        'PASSWORD' : 'django',
+        'HOST' : 'postgres_db',
+        'PORT' : '5432'
     }
 }
 
