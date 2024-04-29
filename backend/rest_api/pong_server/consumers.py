9a95cbca93a7d2e75705759f16dadc2f4519fff0
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2024-04-29 18:49:38 +0200

    Added first draft of token connection in ws

--- backend/rest_api/pong_server/consumers.py
+++ backend/rest_api/pong_server/consumers.py
@@ -7,49 +7,65 @@ from channels.db import database_sync_to_async
 
 connected_clients = set()
 
+class   connect_status:
+    SUCCESS             = 0
+    TOO_MANY_PLAYERS    = 1
+    INVALID_TOKEN       = 2
+    ALREADY_CONNECTED   = 3
+
 class   PongConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
-    def token_connect(self, token):
+    def token_connect(self):
         try :
-            token = Token.objects.get(key=token)
+            token_obj = Token.objects.get(key=self.token)
         except :
-            token = None
-        if token != None :
-            self.user = token.user
+            token_obj = None
+        if token_obj != None :
+            self.user = token_obj.user
             try :
-                game = Game.objects.get(player1=self.user)
+                self.game = Game.objects.get(name=self.room_name)
             except :
-                game = None
-            if game == None :
-                game = Game(player1=self.user.player)
-                game.save()
-            elif game.player2 :
-                game.player2 = self.user.player
+                self.game = None
+            if self.game == None :
+                self.game = Game(name=self.room_name, player1=self.user.player)
+                self.player_no = 1
+                self.game.save()
+            elif self.game.player2 == None:
+                if self.game.player1 == self.game.player2 :
+                    return connect_status.ALREADY_CONNECTED
+                self.game.player2 = self.user.player
+                self.game.save()
+                self.player_no = 2
             else :
-                return False
-            return True
-        return False
+                return connect_status.TOO_MANY_PLAYERS
+            return connect_status.SUCCESS
+        return connect_status.INVALID_TOKEN
+
+    @database_sync_to_async
+    def fetch_username(self, player):
+        return player.user.username
 
     async def connect(self):
+        self.user = None
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.player_no = self.scope["url_route"]["kwargs"]["player_no"]
-        if self.player_no != 1 and self.player_no != 2:
-            self.close()
-        self.versus_no = 1 if self.player_no == 2 else 2
-        self.adversary_name = f"player{self.versus_no}_{self.room_name}"
-        self.player_name    = f"player{self.player_no}_{self.room_name}"
+        self.token = self.scope["url_route"]["kwargs"]["token"]
         connected_clients.add(self.channel_name)
+        ret = await self.token_connect()
+        if ret != connect_status.SUCCESS:
+            await self.close()
+            return
         await self.channel_layer.group_add(
-            self.player_name, self.channel_name
+            self.user.username, self.channel_name
         )
         await self.accept()
 
     async def disconnect(self, code):
         connected_clients.remove(self.channel_name)
-        await self.channel_layer.group_discard(
-            self.player_name, self.channel_name
-        )
+        if self.user != None :
+            await self.channel_layer.group_discard(
+                self.user.username, self.channel_name
+            )
 
     async def receive(self, text_data=None, bytes_data=None):
         try :
@@ -57,8 +73,12 @@ class   PongConsumer(AsyncWebsocketConsumer):
             obj['connected_clients'] = len(connected_clients)
         except :
             obj = {'type' : 'error', 'message' : 'Invalid JSON'}
+        if self.player_no == 2 :
+            target_name = await self.fetch_username(self.game.player1)
+        else :
+            target_name = await self.fetch_username(self.game.player2)
         await self.channel_layer.group_send(
-            self.adversary_name,
+            target_name,
             {
                 "type" : "send_packet",
                 "message" : json.dumps(obj)
--- backend/rest_api/pong_server/routing.py
+++ backend/rest_api/pong_server/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import PongConsumer
 
 websocket_urlpatterns = [
-                        path("ws/room/<str:room_name>/<int:player_no>", PongConsumer.as_asgi()),
+                        path("ws/room/<str:room_name>/<str:token>", PongConsumer.as_asgi()),
                         ]
