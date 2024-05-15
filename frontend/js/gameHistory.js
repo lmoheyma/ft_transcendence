86342e671b4b35dd1651f6c48716f5aa4d11b67f
lmoheyma <louismoheymani@gmail.com>
2024-05-15 03:11:13 +0200

    ✅ Submit btn dashboard responsive + history responsive

--- frontend/js/gameHistory.js
+++ frontend/js/gameHistory.js
@@ -21,18 +21,21 @@ async function getAccountHistory() {
 async function displayHistory() {
 	const historyList = await getAccountHistory();
 	const tbody = document.getElementById('historyBody');
+    let i = 1;
 
 	historyList.forEach(function(game) {
-        const tr = document.createElement('tr');
-        const oponent = document.createElement('td');
-        oponent.classList.add('oponent');
-        oponent.textContent = "vs " + game['player1'];
-        const score = document.createElement('td');
-        score.classList.add('score');
-        score.textContent = game['score_player1']
-            + " - " + game['score_player2'];
-        tr.appendChild(oponent);
-        tr.appendChild(score);
-        tbody.appendChild(tr);
+        if (i++ < historyList.length) {
+            const tr = document.createElement('tr');
+            const oponent = document.createElement('td');
+            oponent.classList.add('oponent');
+            oponent.textContent = "vs " + game['player1'];
+            const score = document.createElement('td');
+            score.classList.add('score');
+            score.textContent = game['score_player1']
+                + " - " + game['score_player2'];
+            tr.appendChild(oponent);
+            tr.appendChild(score);
+            tbody.appendChild(tr);
+        }
     });
 }
